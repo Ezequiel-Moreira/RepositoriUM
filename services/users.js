e63abe9593e570392e2fb2489bedfacf7c23ed8e
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-04-20 17:31:52 +0100

    sixth commit

---
+++ services/users.js
@@ -0,0 +1,48 @@
+const { User } = require( './database' );
+const uid = require( 'uid-safe' );
+const sha = require( 'sha.js' );
+
+class UsersManager {
+  	create ( username, password, group, callback ) {
+      	User.find( { username: username }, ( err, users ) => {
+          	if ( err ) {
+              return callback( err );
+            }
+
+          	if ( users.length > 0 ) {
+            	return callback( new Error( 'Duplicated username: ' + username ) );
+            }
+
+        	  const salt = uid.sync( 10 );
+
+            var hash = sha(  'sha256' ).update( user.salt + password ).digest( 'hex' );
+
+            const new_user=new User({username:username,password:hash,group:group,salt:salt});
+
+          	new_user.save( callback );
+        } );
+    }
+
+    static update ( username, password, group, callback ) {
+    	User.findOne( { username }, ( err, user ) => {
+        	if ( err ) {
+            	return callback( err );
+            }
+          	if ( !user ) {
+            	return callback( new Error( 'Trying to update a non-existent user: ' + username ) );
+            }
+
+            if ( password ) {
+            	const hash = sha( 'sha256' ).update( salt + password ).digest( 'hex' );
+
+              	user.password = hash;
+            }
+
+            if( group ){
+              user.group=group;
+            }
+
+          	user.save( callback );
+        } );
+    }
+}
