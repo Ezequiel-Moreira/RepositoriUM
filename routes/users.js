6793fb2553ef09b90ce760588f875548812ad2a6
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-04-23 16:34:41 +0100

    twelth commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
+// routes/users.js
+
 var express = require('express');
 var router = express.Router();
+var { allowGroups } = require( '../services/login.js' )
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', allowGroups( [ 'admin' ] ), (req, res, next) => {
+		const usersPerPage = 10;
+    const currentPage = +( req.query.page || 0 );
+  	let query = {};
+
+  	User.find( query ).sort({username:1}).skip( currentPage * usersPerPage ).limit( usersPerPage ).exec( ( err, users ) => {
+    	if ( err ) {
+        	return next( err );
+        }
+      	res.render( 'users/list', {
+          users: users,
+          currentPage: currentPage,
+          hasNextPage: users.length == usersPerPage,
+          hasPreviousPage: currentPage > 0
+        } );
+    } );
+} );
 
 module.exports = router;
